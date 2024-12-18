9f64b710166b805ef7e589687389b1ef4f28f582
Omkar Yogesh Tarle <tarleomkar8@gmail.com>
2024-12-19 00:00:51 +0530

    updted all code files

--- src/constants/index.js
+++ src/constants/index.js
@@ -10,13 +10,13 @@ export const projects = [
     title: 'AI Image Generator',
     description: 'A platform for generating AI-powered images.',
     tech: ['React', 'Node.js', 'OpenAI API'],
-    link: 'https://github.com/tarleomkar/Ai-Image-Genrator'
+    link: 'https://statuesque-cajeta-87076a.netlify.app/'
   },
   {
     title: 'Cryptocurrency Tracker',
     description: 'Tracks live crypto prices and trends.',
     tech: ['React', 'Javascript', 'CoinGecko API'],
-    link: 'https://github.com/tarleomkar/Cryptocurrency--Tracker'
+    link: 'https://cryptocurrencytrackerwebsite.netlify.app/'
   },
   {
     title: 'Node-Express-Hotel-CRUD',
@@ -30,4 +30,16 @@ export const projects = [
     tech: ['Javascript', 'React.js', 'Email.js'],
     link: 'https://github.com/tarleomkar/Portfolio-Website'
   },
+  {
+    title: 'E-Commerce Landing Page',
+    description: 'Ecomerce products as you need!',
+    tech: ['Javascript', 'React.js', 'Tailwand.css'],
+    link: 'https://nimble-chebakia-af330d.netlify.app/'
+  },
+  {
+    title: 'Stock Market Analaysis Website',
+    description: 'Provides you real time indicators and stock predictions!',
+    tech: ['Html 5', 'CSS 3', 'Javascript', 'APIs'],
+    link: 'https://stockmarketanalysiswebsite.netlify.app/economiccalender'
+  }
 ];
